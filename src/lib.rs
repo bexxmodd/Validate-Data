b98a20ec02148f601c295c6f4feedfd55f61d3f9
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-07-05 08:13:29 -0400

    cards validator finished. still needs to be tested and documentation added

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub trait Card {
-    fn _validate(number: &str) -> bool {
+    fn check_sum(number: &str) -> bool {
         let digits: Vec<_> = number.chars().into_iter().map(|x| x.to_digit(10)).collect();
 
         let mut even = 0;
@@ -22,15 +22,65 @@ pub trait Card {
         (odd + even) % 10 == 0
     }
 
-    fn check_card(number: &str) -> bool;
+    fn validate(number: &str) -> bool;
 }
 
 struct Visa;
+struct MasterCard;
+struct AmEx;
+struct Discovery;
+struct UnionPay;
+struct Diners;
+struct JCB;
 
 impl Card for Visa {
-    fn check_card(number: &str) -> bool {
+    fn validate(number: &str) -> bool {
         let re = Regex::new(r"^4").unwrap();
-        Visa::_validate(number) && number.len() == 16 && re.is_match(number)
+        Visa::check_sum(number) && number.len() == 16 && re.is_match(number)
+    }
+}
+
+impl Card for MasterCard {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new(r"^(51|52|53|54|55|22|23|24|25|26|27)").unwrap();
+        MasterCard::check_sum(number) && number.len() == 16 && re.is_match(number)
+    }
+}
+
+impl Card for AmEx {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new(r"^(34|37)").unwrap();
+        AmEx::check_sum(number) && number.len() == 15 && re.is_match(number)
+    }
+}
+
+impl Card for Discovery {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new(r"^(60|64|65)").unwrap();
+        Discovery::check_sum(number) && number.len() == 16 && re.is_match(number)
+    }
+}
+
+impl Card for Diners {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new(r"^(30|36|38|39)").unwrap();
+        UnionPay::check_sum(number)
+            && (number.len() == 14 || number.len() == 16)
+            && re.is_match(number)
+    }
+}
+
+impl Card for UnionPay {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new(r"^62").unwrap();
+        UnionPay::check_sum(number) && number.len() == 16 && re.is_match(number)
+    }
+}
+
+impl Card for JCB {
+    fn validate(number: &str) -> bool {
+        let re = Regex::new("r^(60|64|65)").unwrap();
+        JCB::check_sum(number) && number.len() == 16 && re.is_match(number)
     }
 }
 
@@ -39,12 +89,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn is_working() {
-        assert_eq!(true, Visa::_validate("4242424242424242"));
-    }
-
-    #[test]
     fn visa_validate() {
-        assert_eq!(true, Visa::check_card("4242424242424242"));
+        assert_eq!(true, Visa::validate("4242424242424242"));
     }
 }
